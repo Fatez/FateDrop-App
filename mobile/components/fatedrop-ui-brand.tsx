409a361f476c29d339f4248d2df994efd14fbd11
Fatez <cdotkeeley@gmail.com>
2026-08-24 22:37:50 +0100

    Resolve wordmark header export cleanly

--- mobile/components/fatedrop-ui-brand.tsx
+++ mobile/components/fatedrop-ui-brand.tsx
@@ -1,2 +1,18 @@
-export * from './fatedrop-ui';
+export {
+  ScreenBackground,
+  FateDropBackground,
+  AbstractHero,
+  PageNavigation,
+  SectionHeader,
+  IconButton,
+  FilterChip,
+  StatCard,
+  StatusBadge,
+  ActivityCard,
+  ActivityItem,
+  ProductCard,
+  EmptyWatchlistState,
+  statusColors,
+} from './fatedrop-ui';
+export type { StatusTone } from './fatedrop-ui';
 export { FateDropHeader } from './fatedrop-wordmark-header';
