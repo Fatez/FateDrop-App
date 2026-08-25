b52537ce185388100bdbfc8fc43e9956381c8903
Fatez <cdotkeeley@gmail.com>
2026-08-25 15:44:34 +0100

    Use static asset ids for profile catalogue

--- mobile/constants/profile-customisation.ts
+++ mobile/constants/profile-customisation.ts
@@ -1,15 +1,13 @@
-import type { ImageSource } from 'expo-image';
-
 import type { ProfileAvatarId, ProfileWallpaperId } from '@/services/profile-customisation';
 
-export const profileAvatarSources: Record<Exclude<ProfileAvatarId, 'mark'>, ImageSource> = {
+export const profileAvatarSources: Record<Exclude<ProfileAvatarId, 'mark'>, number> = {
   oru: require('../assets/images/profile-oru.png'),
   fenn: require('../assets/images/profile-fenn.png'),
   koru: require('../assets/images/profile-koru.png'),
   nyxen: require('../assets/images/profile-nyxen.png'),
 };
 
-export const profileWallpaperSources: Record<ProfileWallpaperId, ImageSource> = {
+export const profileWallpaperSources: Record<ProfileWallpaperId, number> = {
   oru: require('../assets/images/profile-wallpaper-oru.jpg'),
   fenn: require('../assets/images/profile-wallpaper-fenn.jpg'),
   koru: require('../assets/images/profile-wallpaper-koru.jpg'),
